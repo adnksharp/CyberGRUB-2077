d55491d3e54af62976ce49228f58d7f728718277
adnksharp <darkmark404@protonmail.com>
2025-07-06 18:06:58 -0600

    test (install.sh): Add options for commented GRUB_THEME

--- install.sh
+++ install.sh
@@ -140,14 +140,15 @@ else
 fi
 
 # Modify GRUB
+GRUB_THEME_PATH="GRUB_THEME=\"${THEME_DIR}/${THEME_NAME}/theme.txt\""
 printf "$LNG_EDIT_CHECK"
 # sleep 4
-if grep -q "GRUB_THEME=" "$GRUB_CFG"; then
-    sed -i "s|^GRUB_THEME=.*|GRUB_THEME=\"${THEME_DIR}/${THEME_NAME}/theme.txt\"|" "$GRUB_CFG"
+if grep -qE "^#?GRUB_THEME=" "$GRUB_CFG"; then
+    sed -i -E "s|^#?GRUB_THEME=.*|$GRUB_THEME_PATH|" "$GRUB_CFG"
 else
     # Added extra line before the GRUB_THEME line
     echo "" >> "$GRUB_CFG"
-    echo "GRUB_THEME=\"${THEME_DIR}/${THEME_NAME}/theme.txt\"" >>"$GRUB_CFG"
+     echo "$GRUB_THEME_PATH" >> "$GRUB_CFG"
 fi
 printf "$LNG_EDIT_OK"
 
