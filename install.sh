08c70a92c836e8e9fa444e5062a8e94da6b6ae9c
adnksharp <darkmark404@protonmail.com>
2025-11-23 06:30:33 -0700

    test (install.sh): Add except for grub2-mkconfig

--- install.sh
+++ install.sh
@@ -175,16 +175,30 @@ printf "$LNG_EDIT_OK"
 
 # Updating GRUB
 printf "$LNG_UP_CHECK"
-if command -v grub-mkconfig > /dev/null 2>&1; then
-    sudo grub-mkconfig -o /boot/grub/grub.cfg > /dev/null 2>&1
-    if [ $? -ne 0 ]; then
-        printf "$LNG_UP_FAIL"
-        exit 1
-    fi
-    printf "$LNG_UP_OK"
+if [[ "$DISTRO" == "fedora" || "$DISTRO" == "centos" || "$DISTRO" == "rhel" || "$DISTRO" == "rocky" || "$DISTRO" == "almalinux" ]]; then
+	if command -v grub2-mkconfig > /dev/null 2>&1; then
+		sudo grub2-mkconfig -o /boot/grub2/grub.cfg > /dev/null 2>&1
+		if [ $? -ne 0 ]; then
+			printf "$LNG_UP_FAIL"
+			exit 1
+		fi
+		printf "$LNG_UP_OK"
+	else
+		printf "$LNG_NO_GRUB"
+		exit 1
+	fi
 else
-    printf "$LNG_NO_GRUB"
-    exit 1
+	if command -v grub-mkconfig > /dev/null 2>&1; then
+		sudo grub-mkconfig -o /boot/grub/grub.cfg > /dev/null 2>&1
+		if [ $? -ne 0 ]; then
+			printf "$LNG_UP_FAIL"
+			exit 1
+		fi
+		printf "$LNG_UP_OK"
+	else
+		printf "$LNG_NO_GRUB"
+		exit 1
+	fi
 fi
 
 printf "$LNG_FINISH"
