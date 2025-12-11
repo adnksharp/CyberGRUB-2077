ea86854123728871be7f51f8fb81bcbddffa079f
adnksharp <darkmark404@protonmail.com>
2025-12-10 22:28:09 -0700

    test (install.sh): change gtub2-mkconfig set

--- install.sh
+++ install.sh
@@ -182,15 +182,14 @@ printf "$LNG_EDIT_OK"
 printf "$LNG_UP_CHECK"
 if command -v grub2-mkconfig > /dev/null 2>&1; then
 	sudo grub2-mkconfig -o /boot/${GRUB_ALIAS}/grub.cfg > /dev/null 2>&1
-	exit 1
 	if [ $? -ne 0 ]; then
 		printf "$LNG_UP_FAIL"
 		exit 1
 	fi
 	printf "$LNG_UP_OK"
 else
-	printf "$LNG_NO_GRUB"
 	exit 1
+	printf "$LNG_NO_GRUB"
 fi
 
 printf "$LNG_FINISH"
